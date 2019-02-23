661767d53b45f988ad3e20e30163a62c0624d457
nukopy <pytwbf201830@gmail.com>
2019-02-23 20:25:16 +0900

    solved problems

---
+++ test_code/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+using ll = long long;
+
+int main() {
+    /* アルファベットを 0-origin の数値に変換
+     * ch - 'a' でよい
+    */
+    int num = 'z' - 'a';
+    cout << 'a' - 'a' << "\n";
+    cout << 'c' - 'a' << "\n";
+    cout << 'g' - 'a' << "\n";
+    cout << '1' - '0' << "\n";
+    return 0;
+}
